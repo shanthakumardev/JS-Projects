0e45cb6bff3b7afe1795612c6cd2666ff980670b
shanthakumardev <60182558+shanthakumardev@users.noreply.github.com>
2020-03-18 16:46:51 +0530

    shoeteen the code-best practice

--- Student_Database/js/student.js
+++ Student_Database/js/student.js
@@ -28,39 +28,56 @@ function addStudent(e) {
     window.localStorage.setItem('students', JSON.stringify(students));
 
     addData(student);
+    document.contact-form.submit();
+    document.contact-form.reset();
 }
 
 function addData(student) {
     var parentDiv = document.createElement('div');
+    // used to create an element
+    // var nameSpan = document.createElement('span');
+    // nameSpan.innerHTML = student.name;
+    // nameSpan.setAttribute('class','content');
+    // parentDiv.appendChild(nameSpan);
 
-    var nameSpan = document.createElement('span');
-    nameSpan.innerHTML = student.name;
-    nameSpan.setAttribute('class','content');
-    parentDiv.appendChild(nameSpan);
+    createElem('span',student, 'name', parentDiv);
 
-    var addressSpan = document.createElement('span');
-    addressSpan.innerHTML = student.address;
-    addressSpan.setAttribute('class', 'content');
-    parentDiv.appendChild(addressSpan);
+    // var addressSpan = document.createElement('span');
+    // addressSpan.innerHTML = student.address;
+    // addressSpan.setAttribute('class', 'content');
+    // parentDiv.appendChild(addressSpan);
+
+    createElem('span', student, 'address', parentDiv);
 
     document.getElementById('studentDetails').appendChild(parentDiv);
 }
 
 function displayData() {
-
+    document.contact-form.submit();
+    document.contact-form.reset();
     for(var i =0; i< students.length; i++) {
         var parentDiv = document.createElement('div');
 
-        var nameSpan = document.createElement('span');
-        nameSpan.innerHTML = students[i].name;
-        nameSpan.setAttribute('class','content');
-        parentDiv.appendChild(nameSpan);
+        // var nameSpan = document.createElement('span');
+        // nameSpan.innerHTML = students[i].name;
+        // nameSpan.setAttribute('class','content');
+        // parentDiv.appendChild(nameSpan);
+        createElem('span',students[i], 'name', parentDiv);
 
-        var addressSpan = document.createElement('span');
-        addressSpan.innerHTML = students[i].address;
-        addressSpan.setAttribute('class', 'content');
-        parentDiv.appendChild(addressSpan);
+        // var addressSpan = document.createElement('span');
+        // addressSpan.innerHTML = students[i].address;
+        // addressSpan.setAttribute('class', 'content');
+        // parentDiv.appendChild(addressSpan);
+
+        createElem('span', students[i], 'address', parentDiv);
 
         document.getElementById('studentDetails').appendChild(parentDiv);
     }   
+}
+
+function createElem(element, obj, prop, parent) {
+    var ele = document.createElement(element);   //document.createElement('span');
+    ele.innerHTML = obj[prop];
+    ele.setAttribute('class', 'content');
+    parent.appendChild(ele);
 }
